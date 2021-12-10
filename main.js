dfab3226a03b46507fa8fbfc4fc232dbb298b97a
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2021-12-10 22:46:03 +0900

    JS: home contact

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict'
 
+// Navbar
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener("scroll", () => {
 	if (window.scrollY > navbarHeight) {
 		navbar.classList.add("navbar--dark");
@@ -10,3 +10,25 @@ document.addEventListener("scroll", () => {
 		navbar.classList.remove("navbar--dark");
 	}
 })
+
+// Handle scrolling
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+	const target = e.target;
+	const link = target.dataset.link;
+	if (!link) {
+		return;
+	}
+	scrollIntoView(link);
+})
+
+// Handle Scrolling to Contact me
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", () => {
+	scrollIntoView("#contact")
+})
+
+function scrollIntoView(selector) {
+	const scrollTo = document.querySelector(selector);
+	scrollTo.scrollIntoView({behavior: "smooth"});
+}
