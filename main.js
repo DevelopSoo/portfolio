7777df96c9c6ab6f268bd8f76034046dfc056dcb
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2021-12-13 21:38:08 +0900

    JS: projects animation

--- main.js
+++ main.js
@@ -64,15 +64,22 @@ workBtnContainer.addEventListener("click", (e) => {
 		return;
 	}
 
-	projects.forEach((project) => {
-		if (filter === "*" || filter === project.dataset.type) {
-			project.classList.remove("invisible");
-		} else {
-			project.classList.add("invisible");
-		}
-	})
-
+	projectContainer.classList.add("animation-out");
+	
+	setTimeout(() => {
+		projects.forEach((project) => {
+			if (filter === "*" || filter === project.dataset.type) {
+				project.classList.remove("invisible");
+			} else {
+				project.classList.add("invisible");
+			}
+		})
+		projectContainer.classList.remove("animation-out");
+	}, 300)
+	
 })
+
+
 
 function scrollIntoView(selector) {
 	const scrollTo = document.querySelector(selector);
